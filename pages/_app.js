a0e7038158b16e709e34efe91af05d660b58320d
YeaGram <managerdims@gmail.com>
2022-10-28 21:51:41 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [showChild, setShowChild] = useState(false);
+
+  useEffect(() => {
+    setShowChild(true);
+    document.body.setAttribute("class", "dark:bg-black dark:text-white");
+  }, []);
+  if (!showChild) {
+    return null;
+  }
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
